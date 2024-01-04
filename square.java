2dbe649332c9424531f7c1c9c0c83dc150dca70b
Shaikdilnawaz <154993930+Shaikdilnawaz27@users.noreply.github.com>
2024-01-04 13:37:50 +0530

    Update and rename square,java.txt to square.java

---
+++ square.java
@@ -0,0 +1,29 @@
+question;
+
+read a number and square root of anumber.......
+
+ANS:
+import java.util.*;
+
+public class Main
+
+{
+	
+public static void main(String[] args) 
+	
+{
+	    
+Scanner s=new Scanner(System.in);
+	    
+double a,b;
+	   
+ System.out.println("Enter a no");
+	    a=s.nextDouble();
+	   
+ b=Math.sqrt(a);
+		
+System.out.println(b);
+	
+}
+
+}
